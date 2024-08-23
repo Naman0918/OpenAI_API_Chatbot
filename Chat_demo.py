1724e0c11897faf2e14f90a45bde5484c6f16b9a
Naman <102941774+Naman0918@users.noreply.github.com>
2024-08-23 11:57:25 +0530

    Update Chat_demo.py

--- Chat_demo.py
+++ Chat_demo.py
@@ -3,7 +3,6 @@ import os
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# ChatCompletion is used to generate a response to a prompt not Completion.
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[{"role": "user", "content": "Give me 3 idea to learn ML "}]
